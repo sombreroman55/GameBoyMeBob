8d9be97a9eeb572cfeafd8154d0f539ba476777a
Andrew Roberts <sombreroman55@gmail.com>
2024-11-19 03:18:51 -0700

    Beginning imgui version of main rendering loop

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GameBoyMeBob
 Yet another C++ Game Boy emulator
+
+For the time being, this is only going to target the DMG. I may return to this project in the future
+to implement CGB support as well.
--- main.cc
+++ main.cc
@@ -1,143 +1,185 @@
-// Simple main file to start running Blargg files
+// Main ImGui renderer
+// Probably will get rather large
+
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_memory_editor.h"
+#include <SDL.h>
+#include <cstdio>
+#include <memory>
+#include <vector>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL_opengles2.h>
+#else
+#include <SDL_opengl.h>
+#endif
+
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
 
 #include "cartridge.hh"
 #include "gb.hh"
-#include <string>
-
-#include <SDL2/SDL.h>
-#include <chrono>
 #include <iostream>
+using namespace gameboymebob;
+
+// Globals so we can easily inspect them
+Cartridge* cart = nullptr;
+GameBoy* gb = nullptr;
+
+void draw_memory(void)
+{
+    static bool show_memory = false;
+    static MemoryEditor mem_editor;
+    if (show_memory) {
+        // mem_editor.DrawWindow("Memory", gb->get_memory(), 0x10000);
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* viewport_texture;
-
-    constexpr int FRAMERATE_MS = 1000 / 60;
-    constexpr int SCREEN_HEIGHT = 144;
-    constexpr int SCREEN_WIDTH = 160;
-
-    std::chrono::steady_clock::time_point frame_start;
-    std::chrono::steady_clock::time_point frame_end;
-
-    uint32_t pixel_buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
-    std::string window_title = "GameBoyMeBob";
-
-    //                      #0F380F,  #306230,  #8BAC0F,  #9BBC0F,  #11DDAA };
-    uint32_t colors[5] = { 0x9BBC0F, 0x8BAC0F, 0x306230, 0x0F380F, 0x11DDAA };
-
-    SDL_Init(SDL_INIT_VIDEO);
-
-    window = SDL_CreateWindow(
-        window_title.c_str(),
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN);
-
-    SDL_SetWindowMinimumSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
-    SDL_SetWindowTitle(window, window_title.c_str());
-
-    const int FIRST_AVAILABLE_DRIVER = -1;
-    renderer = SDL_CreateRenderer(
-        window,
-        FIRST_AVAILABLE_DRIVER,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-    viewport_texture = SDL_CreateTexture(
-        renderer,
-        SDL_PIXELFORMAT_ARGB8888,
-        SDL_TEXTUREACCESS_STREAMING,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT);
-
-    std::string rom_file(argv[1]);
-
-    gameboymebob::GameBoy* gb = new gameboymebob::GameBoy();
-    gameboymebob::Cartridge* cart = new gameboymebob::Cartridge(rom_file);
-    gb->insert_cartridge(cart);
-
-    frame_start = std::chrono::steady_clock::now();
-    bool done = false;
-    SDL_Event ev;
-    while (!done) {
-        gb->run();
-        if (gb->frame_ready()) {
-            while (SDL_PollEvent(&ev)) {
-                if (ev.type == SDL_QUIT) {
-                    done = true;
-                } else if (ev.type == SDL_KEYDOWN) {
-                    gameboymebob::Button button = gameboymebob::Button::Invalid;
-                    switch (ev.key.keysym.sym) {
-                    // clang-format off
-                    case SDLK_UP:           button = gameboymebob::Button::Up;      break;
-                    case SDLK_DOWN:         button = gameboymebob::Button::Down;    break;
-                    case SDLK_LEFT:         button = gameboymebob::Button::Left;    break;
-                    case SDLK_RIGHT:        button = gameboymebob::Button::Right;   break;
-                    case SDLK_x:            button = gameboymebob::Button::A;       break;
-                    case SDLK_z:            button = gameboymebob::Button::B;       break;
-                    case SDLK_BACKSPACE:    button = gameboymebob::Button::Select;  break;
-                    case SDLK_RETURN:       button = gameboymebob::Button::Start;   break;
-                    // clang-format on
-                    }
-                    if (button != gameboymebob::Button::Invalid) {
-                        gb->press_button(button);
-                    }
-                } else if (ev.type == SDL_KEYUP) {
-                    gameboymebob::Button button = gameboymebob::Button::Invalid;
-                    switch (ev.key.keysym.sym) {
-                    // clang-format off
-                    case SDLK_UP:           button = gameboymebob::Button::Up;      break;
-                    case SDLK_DOWN:         button = gameboymebob::Button::Down;    break;
-                    case SDLK_LEFT:         button = gameboymebob::Button::Left;    break;
-                    case SDLK_RIGHT:        button = gameboymebob::Button::Right;   break;
-                    case SDLK_x:            button = gameboymebob::Button::A;       break;
-                    case SDLK_z:            button = gameboymebob::Button::B;       break;
-                    case SDLK_BACKSPACE:    button = gameboymebob::Button::Select;  break;
-                    case SDLK_RETURN:       button = gameboymebob::Button::Start;   break;
-                    // clang-format on
-                    }
-                    if (button != gameboymebob::Button::Invalid) {
-                        gb->release_button(button);
-                    }
-                }
-            }
-
-            frame_end = std::chrono::steady_clock::now();
-            auto time_spent = std::chrono::duration_cast<std::chrono::milliseconds>(frame_end - frame_start).count();
-            // std::cout << "Frame took " << time_spent << " ms" << std::endl;
-            if (time_spent < FRAMERATE_MS) {
-                uint32_t sleep_ms = FRAMERATE_MS - time_spent;
-                SDL_Delay(sleep_ms);
-            }
-            frame_start = std::chrono::steady_clock::now();
-
-            uint8_t* frame = gb->get_frame();
-
-            SDL_RenderClear(renderer);
-            void* pixels_ptr;
-            int pitch;
-            SDL_LockTexture(viewport_texture, nullptr, &pixels_ptr, &pitch);
-
-            uint32_t* pixels = static_cast<uint32_t*>(pixels_ptr);
-
-            for (int i = 0; i < SCREEN_HEIGHT; i++) {
-                for (int j = 0; j < SCREEN_WIDTH; j++) {
-                    pixels[i * SCREEN_WIDTH + j] = colors[frame[i * SCREEN_WIDTH + j]];
-                }
-            }
-
-            SDL_UnlockTexture(viewport_texture);
-            SDL_RenderCopy(renderer, viewport_texture, NULL, NULL);
-            SDL_RenderPresent(renderer);
-        }
+    // Setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
     }
 
-    delete gb;
-    SDL_DestroyTexture(viewport_texture);
-    SDL_DestroyRenderer(renderer);
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(__APPLE__)
+    // GL 3.2 Core + GLSL 150
+    const char* glsl_version = "#version 150";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#endif
+
+    // From 2.0.18: Enable native IME.
+#ifdef SDL_HINT_IME_SHOW_UI
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow("GameBoyMeBob", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    if (window == nullptr) {
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
+    // io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    // IM_ASSERT(font != nullptr);
+
+    gb = new GameBoy();
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    bool done = false;
+#ifdef __EMSCRIPTEN__
+    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    while (!done)
+#endif
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                done = true;
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+
+        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
+            SDL_Delay(10);
+            continue;
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
